1ae2633f31e13df657523f3e5102295f5fae39aa
Vladimir Mihailenco <vladimir.webdev@gmail.com>
2023-02-02 14:23:59 +0200

    chore: update dependencies

--- docs/.vuepress/client.ts
+++ docs/.vuepress/client.ts
@@ -1,6 +1,6 @@
 import { defineClientConfig, resolvers } from '@vuepress/client'
 
-import Layout from './layouts/Layout.vue'
+import Layout from '@/layouts/Layout.vue'
 
 export default defineClientConfig({
   layouts: {
