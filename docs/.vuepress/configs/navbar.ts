d87b53726d67a412adb3fd2a312c97c28d7ae634
Vladimir Mihailenco <vladimir.webdev@gmail.com>
2023-02-02 13:38:48 +0200

    some groundwork for chinese translation

--- docs/.vuepress/configs/navbar.ts
+++ docs/.vuepress/configs/navbar.ts
@@ -2,11 +2,18 @@ import type { NavbarConfig } from '@vuepress/theme-default'
 
 export const en: NavbarConfig = [
   {
-    text: 'Newsletter',
-    link: 'https://blog.uptrace.dev/newsletter/',
+    text: 'Reference',
+    link: 'https://pkg.go.dev/github.com/go-redis/redis/v8',
   },
   {
-    text: 'Reference',
+    text: 'GitHub',
+    link: 'https://github.com/go-redis/redis',
+  },
+]
+
+export const zh: NavbarConfig = [
+  {
+    text: '参考',
     link: 'https://pkg.go.dev/github.com/go-redis/redis/v8',
   },
   {
--- docs/.vuepress/configs/sidebar.ts
+++ docs/.vuepress/configs/sidebar.ts
@@ -37,3 +37,41 @@ export const en: SidebarConfig = {
     },
   ],
 }
+
+export const zh: SidebarConfig = {
+  '/': [
+    {
+      isGroup: true,
+      text: 'Guide',
+      children: [
+        { text: '介绍', link: '/guide/' },
+        { text: 'Getting started', link: '/guide/go-redis.html' },
+        { text: 'Redis Cluster', link: '/guide/go-redis-cluster.html' },
+        { text: 'Redis Sentinel', link: '/guide/go-redis-sentinel.html' },
+        { text: 'Redis Ring', link: '/guide/ring.html' },
+        { text: 'Universal client', link: '/guide/universal.html' },
+        { text: 'Pipelines and transactions', link: '/guide/go-redis-pipelines.html' },
+        { text: 'PubSub', link: '/guide/go-redis-pubsub.html' },
+      ],
+    },
+    {
+      isGroup: true,
+      text: 'Tutorial',
+      children: [
+        { text: 'Debugging: pool size, timeouts', link: '/guide/go-redis-debugging.html' },
+        { text: 'Monitoring performance and errors', link: '/guide/go-redis-monitoring.html' },
+        { text: 'Redis Cache', link: '/guide/go-redis-cache.html' },
+        { text: 'Lua scripting', link: '/guide/lua-scripting.html' },
+        { text: 'Rate-limiting', link: '/guide/go-redis-rate-limiting.html' },
+        { text: 'Get all keys', link: '/guide/get-all-keys.html' },
+        { text: 'Scanning hash fields into a struct', link: '/guide/scanning-hash-fields.html' },
+        {
+          text: 'Bloom, Cuckoo, Count-Min, Top-K',
+          link: '/guide/bloom-cuckoo-count-min-top-k.html',
+        },
+        { text: 'HyperLogLog', link: '/guide/go-redis-hll.html' },
+        { text: 'go-redis vs redigo', link: '/guide/go-redis-vs-redigo.html' },
+      ],
+    },
+  ],
+}
